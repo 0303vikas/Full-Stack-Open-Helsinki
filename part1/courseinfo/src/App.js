eb18b14add50bf1393c48b37b8ef968a21621599
0303vikas <2751vikassing@gmail.com>
2021-11-17 00:44:53 +0200

    Part 1 Complete

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+const Header = (props) => {
+  return(
+
+  <>
+  <h1>{props.course}</h1>
+  </>)
+
+};
+
+const ExeNum = (props) => {
+  return(
+    <>
+    <p>
+      {props.part} {props.exercise} 
+    </p>
+
+    </>
+  )
+}
+
+const Content = (props) => {
+  const list = props.exercise;
+  const part = props.part;
+
+  return(
+  <>
+  <ExeNum part={part[0]} exercise={list[0]} />
+  <ExeNum part={part[1]} exercise={list[1]} />
+  <ExeNum part={part[2]} exercise={list[2]} />
+  
+  </>)
+
+};
+
+const Total = (props) => {
+  return(  <>
+  <p>Number of exercise {props.total}</p>
+  </>)
+
+};
+
+const App = () => {
+
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const part = [part1, part2, part3];
+  const exercise = [exercises1, exercises2,exercises3];
+  
+
+  return (
+    <>
+    <Header course={course}/>
+    <Content exercise={exercise} part={part}/>
+    <Total total={exercises1+exercises2+exercises3}/>
+
+
+    
+    
+    
+    
+  </>);
+};
+
+
+
+export default App;
