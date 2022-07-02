ca2b8f0a61a4737e253cbbe056a8d85aaea78f35
0303vikas <2751vikassing@gmail.com>
2022-07-02 12:23:34 +0300

    part2.1-Part2.5

---
+++ part2/Course_information_2.1-2.5/src/App.js
@@ -0,0 +1,64 @@
+
+import './App.css';
+import Course from './view/Course';  // contains all the printed data and logic
+
+
+const App = () => {
+
+  //Collection of all courses and parts under them
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+
+
+    <div>
+      <Course course={courses} /> {/*initates the course function*/}
+     
+    </div>
+  )
+}
+
+export default App;
