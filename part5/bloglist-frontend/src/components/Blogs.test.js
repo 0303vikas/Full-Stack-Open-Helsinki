3eb530e0a8e3a651935b727d71b1300796a339f8
0303vikas <2751vikassing@gmail.com>
2022-09-27 18:02:02 +0300

    5.c.14 checking the blogs after clicking view button

--- part5/bloglist-frontend/src/components/Blogs.test.js
+++ part5/bloglist-frontend/src/components/Blogs.test.js
@@ -2,34 +2,54 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import { Blog } from './Blogs'
+import userEvent from '@testing-library/user-event'
 
-describe('testing the blog render component', () => {
+describe('<Blog />', () => {
+    let element
 
-    // beforeEach(() => {
+    const blog = {
+        title: 'testing the blog',
+        author: 'tester',
+        url: 'http://localhost:3003',
+        likes: 10
+    }
 
-    // })
+    const user = {
+        token: 'ishdio28suimdickds0sie92jj',
+        username: 'vikas',
+    }
+
+    const blogUpdate = () => {
+        console.log('test run')
+    }
+
+    beforeEach(() => {
+        element = render(<Blog blog={ blog } user={ user } blogUpdate={ blogUpdate }  />)
+        screen.debug()
+    })
 
     test('initial view, only the title and the view button are visible', () => {
-        const blog = {
-            title: 'testing the blog',
-            author: 'tester',
-            url: 'http://localhost:3003',
-            likes: 10
-        }
-
-        const user = {
-            token: 'ishdio28suimdickds0sie92jj',
-            username: 'vikas',
-        }
-
-        const blogUpdate = () => {
-            console.log('test run')
-        }
-
-        render(<Blog blog={ blog } user={ user } blogUpdate={ blogUpdate }  />)
         const p = screen.getByText(blog.title)
         screen.debug()
         expect(p).toBeDefined()
+    })
+
+    test('<button>view</button>', async () => {
+        const user = userEvent.setup()
+
+        const viewbutton = screen.getByText('view')
+
+        await user.click(viewbutton)
+
+        screen.debug()
+
+        expect(screen.getByText(blog.url).textContent).toBe(blog.url)
+        expect(screen.getByText(blog.author).textContent).toBe(blog.author)
+        expect(screen.getByText(blog.likes, { exact: false })).toBeInTheDocument()
+
+
+
+
     })
 
 })
